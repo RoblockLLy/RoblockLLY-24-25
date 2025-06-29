38d57e5b70772563ad8e77c6e31efd42e7a3bac1
Thomas Bradley <thomasattenerife@gmail.com>
2025-06-29 20:04:27 +0100

    Search can be triggered with enter key

---
+++ src/Assets/Scripts/Menus/EnterKeySearch.cs
@@ -0,0 +1,27 @@
+/**
+* Universidad de La Laguna
+* Proyecto: Roblockly-Android
+* Autor: Thomas Edward Bradley
+* Email: alu0101408248@ull.edu.es
+* Fecha: 29/06/2025
+* Descripcion: Activa el boton de busqueda cuando se detecta al usuario pulsar la tecla 'Enter'
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterKeyListener : MonoBehaviour {
+  /// <summary>
+  /// Boton a pulsar cuando se detecte la tecla pulsada
+  /// </summary>
+  public Button targetButton;
+
+  /// <summary>
+  /// Revisa cada frame si la tecla se ha pulsado
+  /// </summary>
+  void Update() {
+    if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter)) {
+      targetButton.onClick.Invoke();
+    }
+  }
+}
